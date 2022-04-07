7c60e852c75c609f06a5df8d4d24b7b82f8b17a5
Pin-Yu Wang <vito1218@gmail.com>
2022-04-07 22:37:44 +0800

    Make result observable

--- src/main/java/pinyu/main/Start.java
+++ src/main/java/pinyu/main/Start.java
@@ -11,10 +11,10 @@ public class Start {
 	// FifoWithoutNotifyAll: 0, FifoWithNotifyAll: 1
 	public static final int PATTERN_TYPE = 1;
 	public static final int BENCHMARK_TIME_IN_MS = 5000;
-	public static final int THREAD_SLEEP_TIME_IN_MS = 10;
+	public static final int THREAD_SLEEP_TIME_IN_MS = 1;
 
 	private static ExecutorService executor;
-	private static final int THREAD_NUM = 100;
+	private static final int THREAD_NUM = 10;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
